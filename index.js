939f490f601455f90eee42f9cd3249719e4d6356
Rob Whiting <rob.t.whiting@gmail.com>
2018-06-29 04:02:15 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < musicians.length; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array 
+}
+
+function johnLennonFacts(facts) {
+  let i = facts.length
+  while (i > 0) {
+    console.log(--i)
+    facts[i]=facts[i]+"!!!";
+  }
+  return facts
+}
